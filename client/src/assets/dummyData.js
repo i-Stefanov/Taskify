5107deb9971c0c3c0b7f7a0f2551ad3172c9cd4d
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-11-18 11:33:57 +0200

    add dummy data for testing

---
+++ client/src/assets/dummyData.js
@@ -0,0 +1,43 @@
+export const tasks = [
+  {
+    id: 1,
+    name: "Task 1",
+    dueDate: "2023-12-01",
+    priority: "High",
+    description: "Lorem ipsum...",
+  },
+  {
+    id: 2,
+    name: "Task 2",
+    dueDate: "2023-12-15",
+    priority: "Medium",
+    description: "Lorem ipsum...",
+  },
+  {
+    id: 3,
+    name: "Task 2",
+    dueDate: "2023-12-15",
+    priority: "Medium",
+    description: "Lorem ipsum...",
+  },
+  {
+    id: 4,
+    name: "Task 2",
+    dueDate: "2023-12-15",
+    priority: "Medium",
+    description: "Lorem ipsum...",
+  },
+  {
+    id: 5,
+    name: "Task 2",
+    dueDate: "2023-12-15",
+    priority: "Medium",
+    description: "Lorem ipsum...",
+  },
+];
+export const testUser = {
+  id: 1,
+  username: "john_doe",
+  email: "john@example.com",
+  role: "Admin",
+};
