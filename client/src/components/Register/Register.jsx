c7ccbed873576caa20b9718cd123b9da72d8d712
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-11-18 12:55:28 +0200

    add register

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import styles from "./Register.module.css";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
