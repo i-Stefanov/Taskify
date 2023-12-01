775fe4cf568edc99060ea40508c92f505f48e23b
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-12-01 11:51:56 +0200

    navigate to tasklist instead of edited task

--- client/src/contexts/taskContext.jsx
+++ client/src/contexts/taskContext.jsx
@@ -8,29 +8,34 @@ export const TaskProvider = ({ children }) => {
 
   const taskService = taskServiceFactory();
   const [tasks, setTasks] = useState([]);
+
   useEffect(() => {
     taskService.getAll().then((result) => setTasks(result));
   }, []);
+
   const onCreateTaskSubmit = async (data) => {
     const newTask = await taskService.create(data);
-    console.log(newTask);
     // add the newly created Task to the state
     setTasks((state) => [...state, newTask]);
     // redirect to catalog page use useNavigate from react-router-dom
     navigate("/tasklist");
   };
+
   const onTaskEditSubmit = async (values) => {
     const result = await taskService.edit(values._id, values);
-    //todo change state !!!
+    // change state !!!
     // since the Task is already in the state in some form and we just want to change some of the data about this Task ,we map through the Tasks array and find the one with id that matches the id of the Task that we want to change and then replace it in the state
     setTasks((state) =>
       state.map((task) => (task._id === values._id ? result : task))
     );
-    navigate(`/tasklist/${values._id}`);
+    // navigate(`/tasklist/${values._id}`);
+    navigate("/tasklist");
   };
+
   const getTaskFromState = (taskId) => {
     return tasks.find((task) => task._id === taskId);
   };
+
   const deleteTask = (taskId) => {
     setTasks((tasks) => tasks.filter((task) => task._id !== taskId));
   };
@@ -48,6 +53,7 @@ export const TaskProvider = ({ children }) => {
     </TaskContext.Provider>
   );
 };
+
 export const useTaskContext = () => {
   const context = useContext(TaskContext);
   return context;
